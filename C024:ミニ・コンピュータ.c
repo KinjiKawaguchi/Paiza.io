7646ccce73db23a4e12aba6cf298596a9cc6a4df
河口　欣仁 <93261102+KinjiKawaguchi@users.noreply.github.com>
2021-12-21 15:07:12 +0900

    Update C024:ミニ・コンピュータ.c

--- C024:ミニ・コンピュータ.c
+++ C024:ミニ・コンピュータ.c
@@ -3,36 +3,21 @@
 int main(void){
     int commandN;
     scanf("%d",&commandN);
-    int variable1,variable2;
+    int variable1=0,variable2=0;
     for(int i = 0;i<commandN;i++){
         char command[4];
         int variable,number,check=0;
         scanf("%s",command);
+        if(strcmp(command,"SET")==0) check = 1;   
+        if(check == 1)scanf("%d %d",&variable,&number);
+        else scanf("%d",&number);
         
         if(strcmp(command,"SET")==0){
-            check = 1;   
+            if(variable==1) variable1 = number;
+            else variable2 = number;
         }
-        
-        if(check == 1){
-            scanf("%d %d",&variable,&number);
-        }else{
-            scanf("%d",&number);
-        }
-        
-        if(strcmp(command,"SET")==0){
-            if(variable==1){
-                variable1 = number;
-            }else{
-                variable2 = number;
-            }
-        }
-        
-        else if(strcmp(command,"ADD")==0){
-            variable2 = variable1 + number;
-        }
-        else if(strcmp(command,"SUB")==0){
-            variable2 = variable1 - number;
-        }
+        else if(strcmp(command,"ADD")==0) variable2 = variable1 + number;
+        else if(strcmp(command,"SUB")==0) variable2 = variable1 - number;
     }
     printf("%d %d",variable1,variable2);
 }
