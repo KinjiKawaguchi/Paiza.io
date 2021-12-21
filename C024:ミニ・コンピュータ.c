dd23fe6c38adcac70ebd168687a21bc1513e07af
河口　欣仁 <93261102+KinjiKawaguchi@users.noreply.github.com>
2021-12-22 08:47:53 +0900

    Update C024:ミニ・コンピュータ.c

--- C024:ミニ・コンピュータ.c
+++ C024:ミニ・コンピュータ.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 int main(void){
-    int commandN;
-    scanf("%d",&commandN);
-    int variable1=0,variable2=0;
-    for(int i = 0;i<commandN;i++){
+    int Numberof_command,variable1=0,variable2=0;
+    scanf("%d",&Numberof_command);
+    
+    for(int i = 0;i<Numberof_command;i++){
         char command[4];
         int variable,number,check=0;
         scanf("%s",command);
-        if(strcmp(command,"SET")==0) check = 1;   
-        if(check == 1)scanf("%d %d",&variable,&number);
+        if(strcmp(command,"SET")==0) 
+            scanf("%d %d",&variable,&number);
         else scanf("%d",&number);
         
         if(strcmp(command,"SET")==0){
