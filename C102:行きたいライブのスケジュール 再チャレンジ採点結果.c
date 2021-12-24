a56e6f31d8114b893875ca117600f7d1851f8a48
河口　欣仁 <93261102+KinjiKawaguchi@users.noreply.github.com>
2021-12-25 02:00:56 +0900

    Update C102:行きたいライブのスケジュール 再チャレンジ採点結果.c

--- C102:行きたいライブのスケジュール 再チャレンジ採点結果.c
+++ C102:行きたいライブのスケジュール 再チャレンジ採点結果.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 int main(void){
-    int N,M,which=0;
-    scanf("%d",&N);
-    int A[31],B[31];
-    //初期化
+    int N,M,which=0,A[31],B[31];
     for(int i=0;i<=31;i++){
         A[i]=0;
         B[i]=0;
     }
-    //ライブのある日と添字を一致させる
+    
+    scanf("%d",&N);
     for(int i = 0;i<N;i++){
         scanf("%d",&A[0]);
-        for(int j =1;j<=31;j++) {
+        for(int j = 1;j <=31;j++) {
             if(A[0]==j) A[j]=A[0];
         }
     }
     scanf("%d",&M);
     for(int i = 0;i<M;i++){
         scanf("%d",&B[0]);
-        for(int j =1;j<=31;j++) {
+        for(int j = 1;j <=31;j++) {
             if(B[0]==j) B[j]=B[0];
         }
     }
     
-    //出力
     for(int i=1;i<=31;i++){
         if(A[i] == B[i]&&A[i]!=0&&B[i]!=0){
             if(which==0){
